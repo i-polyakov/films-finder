9bd2023f890ed940720d7fbf6940f356d80f7a3a
i-polyakov <ilya.pollyakov@gmail.com>
2022-06-01 22:25:38 +0300

    update user-based recommendation

--- filmsFinder/server/controllers/main.controller.js
+++ filmsFinder/server/controllers/main.controller.js
@@ -1,57 +1,75 @@
+const Film = require("../models/film");
 const User = require("../models/user");
 
 //косинусная мера 
-function distCosine(fUR, sUR) {//(firstUserRatings, secondUserRatings) dict
+function distCos(fUR, sUR) {//(firstUserRatings, secondUserRatings) dict
     function dotProduct(fUR, sUR){ //скалярное произведение       
-    d = 0.0;
-    fUR.forEach(elem => {  
-        isfind = sUR.find((item) => {           
-            return String(item.filmId) == String(elem.filmId);
-        });   
-        if (isfind){  
-            d += elem.rating * isfind.rating;
-        }          
-    });
-   
-    return d
- 
+        d = 0.0;
+        fUR.forEach(elem => {  
+            isfind = sUR.find((item) => {           
+                return String(item.filmId) == String(elem.filmId);
+            });   
+            if (isfind) 
+                d += elem.rating * isfind.rating;                
+        });
+        return d
     }   
     return dotProduct (fUR,sUR) / Math.sqrt(dotProduct(fUR,fUR)) / Math.sqrt(dotProduct(sUR,sUR))
 }
 
-function makeRecommendation (curentUser, allUsers, countBestSimilars, nBestProducts){
-    similars = [];//матрица "похожести" curentUser и allUsers 
+function recommendedFilms (curentUser, allUsers, countBestSimilars, countRecommendedFilms){
+    //матрица "соседей" curentUser  
+    similars = [];
+
+    // заполнение матрицы similars
     allUsers.forEach(user => {
         if(user.id != curentUser.id)
-            similars.push({id: user.id, login: user.login, similarity: distCosine(curentUser.watched, user.watched)})
+            similars.push({watched: user.watched, login: user.login, similarity: distCos(curentUser.watched, user.watched)})
     });
-    
+    //countBestSimilars наиболее похожих
     bestSimilars = similars.sort((a, b) => (a.similarity > b.similarity) ? 1 : ((b.similarity > a.similarity) ? -1 : 0))
                          .reverse()
-                         .slice(0, countBestSimilars);                    
+                         .slice(0, countBestSimilars);
+    //сумма мер                     
+    similarsSum = 0
     bestSimilars.forEach(element => {
-        console.log(element);
-    });                        
-    // sim = dict()
-    // sim_all = sum([x[1] for x in bestMatches])
-    // bestMatches = dict([x for x in bestMatches if x[1] > 0.0])        
-    // for relatedUser in bestMatches:
-    //     for product in userRates[relatedUser]:
-    //         if not product in userRates[userID]:
-    //             if not product in sim:
-    //                 sim[product] = 0.0
-    //             sim[product] += userRates[relatedUser][product] * bestMatches[relatedUser]
-    // for product in sim:
-    //     sim[product] /= sim_all
-    // bestProducts = sorted(sim.iteritems(), key=lambda(x,y):(y,x), reverse=True)[:nBestProducts]
-    // print "Most correlated products:"
-    // for prodInfo in bestProducts:    
-    //     print "  ProductID: %6s  CorrelationCoeff: %6.4f" % (prodInfo[0], prodInfo[1])
-    // return [(x[0], x[1]) for x in bestProducts]
+        similarsSum += element.similarity
+    });
+    //сумма калиброванных оценок по фильмам
+    sum = []    
+    bestSimilars.forEach(elem => {
+        elem.watched.forEach(element => {
+            element.rating *= elem.similarity
+            sum[String(element.filmId)] ? sum[String(element.filmId)] += element.rating : sum[String(element.filmId)] = element.rating
+    
+        });        
+    });
+    //исключить фильмы которые curentUser смотрел
+    curentUser.watched.forEach(element => {
+        delete sum[String(element.filmId)]
+    });
+
+    recFilms = []
+    for (const key in sum) {
+        if (Object.hasOwnProperty.call(sum, key)) {
+            recFilms.push({filmId: key, value: sum[key]/Math.abs(similarsSum)})       
+        }
+    }
+    recFilms = recFilms.sort( (a, b) => {return a.value - b.value })
+                                        .reverse()
+                                        .slice(0,countRecommendedFilms)
+    console.table(recFilms);
+    filmId = []
+    recFilms.forEach(element => {
+        filmId.push(element.filmId)
+    });
+    
+    console.log(filmId );
+    return   filmId;
+
 }
 class MainController {
-
-     
+    
     async getTopFilms(req, res){
 
     }
@@ -69,8 +87,11 @@ class MainController {
                 table[element.login] = films;        
             });
             console.table(table)
-            makeRecommendation(curentUser,allUsers,4,0)
-            return res.json("")
+            const recommendation =  recommendedFilms(curentUser, allUsers, 4, 3)
+            //console.log(recommendation);
+            const films = await Film.find({ '_id': {$in: recommendation}})
+            console.log(films)
+            return res.json(films)
           } catch (error) {
             console.log(error);
           }  
