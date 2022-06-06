3200a16166ad2f4efa4b4c2ee58c822cbfe6a7c5
i-polyakov <ilya.pollyakov@gmail.com>
2022-06-06 14:06:10 +0300

    add similarUser func

--- filmsFinder/server/controllers/main.controller.js
+++ filmsFinder/server/controllers/main.controller.js
@@ -16,10 +16,116 @@ function distCos(fUR, sUR) {//(firstUserRatings, secondUserRatings) dict
     }   
     return dotProduct (fUR,sUR) / Math.sqrt(dotProduct(fUR,fUR)) / Math.sqrt(dotProduct(sUR,sUR))
 }
+// Возвращает коэффициент корреляции Пирсона между user1 и user2
+function similarUser(u1, u2){
+    //список фильмов, оцененных обоими
+    const filmsId = []
+    //Суммы оценок фильмов
+    sum1 = 0;
+    sum2 = 0;
+    //Cуммы квадратов
+    sum1Pow = 0;
+    sum2Pow = 0;
+    //сумма произведений
+    sumM = 0;
+    //Получить список фильмов, оцененных обоими
+    u1.watched.forEach(filmU1 => {
+        isfind = u2.watched.find(filmU2 => {           
+            return (String(filmU1.filmId) == String(filmU2.filmId) && filmU1.rating && filmU2.rating );
+        });
+        //Если фильм найден, то вычислить суммы оценок фильмов, суммы квадратов и сумму произведений
+        if (isfind){
+            filmsId.push(isfind.filmId);   
+
+            sum1 += filmU1.rating;
+            sum2 += isfind.rating;
+
+            sum1Pow += filmU1.rating**2;
+            sum2Pow += isfind.rating**2;
+
+            sumM += filmU1.rating * isfind.rating;
+        }      
+    });
+    //Если нет ни одной общей оценки, вернуть 0
+    if(!filmsId.length)
+        return 0;
+    //Вычислить коэффициент Пирсона
+    n = filmsId.length;    
+    numerator = n * sumM  - sum1 * sum2
+    denominator = (n * sum1Pow - sum1**2)**(1/2) * (n * sum2Pow - sum2**2)**(1/2)
+    if(!denominator)
+        return 0;    
+    return numerator / denominator; 
+}
+function similarUser2(u1, u2){
+    //список фильмов, оцененных обоими
+    const filmsId = []
+    //Суммы оценок фильмов
+    sum1 = 0;
+    sum2 = 0;
+    //Cуммы квадратов
+    sum1Pow = 0;
+    sum2Pow = 0;
+    //сумма произведений
+    sumM = 0;
+    //Получить список фильмов, оцененных обоими
+    srd1 = 0 ;
+    i1 = 0;
+    i2 = 0;
+    srd2 = [];
+    u1.watched.forEach(film => {
+        if(film.rating){
+            srd1 += film.rating;
+            i1++;
+        }
+    });
+    u2.watched.forEach(film => {
+        if(film.rating){
+            srd2 += film.rating;
+            i2++;
+        }
+          
+    });
+    if(i1)
+        srd1 /= i1
+    if(i2)
+        srd2 /= i2    
+    
+    u1.watched.forEach(filmU1 => {
+        isfind = u2.watched.find(filmU2 => {           
+            return (String(filmU1.filmId) == String(filmU2.filmId) && filmU1.rating && filmU2.rating );
+        });
+        //Если фильм найден, то вычислить суммы оценок фильмов, суммы квадратов и сумму произведений
+        if (isfind){
+            filmsId.push(isfind.filmId);   
+
+            sum1 += filmU1.rating;
+            sum2 += isfind.rating;
+
+            sum1Pow += (filmU1.rating - srd1)**2;
+            sum2Pow += (isfind.rating - srd2)**2;
+
+            sumM += (filmU1.rating - srd1) * (isfind.rating - srd2) ;
+        }      
+    });
+    //Если нет ни одной общей оценки, вернуть 0
+    if(!filmsId.length)
+        return 0;
+    //Вычислить коэффициент Пирсона
+    n = filmsId.length;    
+    denominator = (sum1Pow)**(1/2) * (sum2Pow)**(1/2)
+    if(!denominator)
+        return 0;    
+    return sumM / denominator; 
+}
 
 function recommendedFilms (curentUser, allUsers, countBestSimilars, countRecommendedFilms){
     //матрица "соседей" curentUser  
     similars = [];
+    allUsers.forEach(user => {
+        if(user.id != curentUser.id)
+            console.log(user.login+": " +  distCos(curentUser.watched, user.watched)+" "+ similarUser(curentUser, user)+" "+ similarUser2(curentUser, user) )
+    });
 
     // заполнение матрицы similars
     allUsers.forEach(user => {
